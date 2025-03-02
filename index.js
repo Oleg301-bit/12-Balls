2bb49f0be704d95cc5b2fd0fe68c2ab681dda509
Oleg <olegmaslakov47@gmail.com>
2025-03-02 23:20:48 +0200

    Update js code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 "use strict";
-const outerDiameter = 0.5;
-const wallThickness = 0.01;
-const allBalls = 12;
+const Outer_Diameter = 0.5;
+const Wall_Thickness = 0.01;
+const All_Balls = 12;
 
-let outerRadius = outerDiameter / 2;
+let outerRadius = Outer_Diameter / 2;
 let vTotal = 0;
 
-for (let firstBall = 1; firstBall <= allBalls; firstBall++) {
-  let innerRadius = outerRadius - wallThickness;
+for (let firstBall = 1; firstBall <= All_Balls; firstBall++) {
+  let innerRadius = outerRadius - Wall_Thickness;
   vTotal += (4 / 3) * Math.PI * Math.pow(innerRadius, 3);
-  outerRadius += wallThickness;
+  outerRadius += Wall_Thickness;
 }
 console.log(vTotal.toFixed(4), "м^3");
